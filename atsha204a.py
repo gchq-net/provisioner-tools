3e135f3909adbab85063d8f6acd16f1d9fe07d88
Tyler Ward <tyler@scorpia.co.uk>
2024-05-12 13:41:03 +0100

    Split ATSHA inital test code into usefull functions

--- atsha204a.py
+++ atsha204a.py
@@ -1,9 +1,33 @@
+import enum
 import pyftdi.i2c
 import time
 
 
+class atasha204A_command(enum.Enum):
+    DERIVE_KEY = 0x1C
+    DEV_REV = 0x30
+    GEN_DIG = 0x15
+    HMAC = 0x11
+    CHECK_MAC = 0x28
+    LOCK = 0x17
+    MAC = 0x08
+    NONCE = 0x16
+    PAUSE = 0x01
+    RANDOM = 0x1B
+    READ = 0x02
+    SHA = 0x47
+    UPDATE_EXTRA = 0x20
+    WRITE = 0x12
+
+
+class atasha204A_zone(enum.Enum):
+    CONFIG = 0x00
+    OTP = 0x01
+    DATA = 0x02
+
+
 class atsha204A:
-    i2c_port = None
+    i2c_port: pyftdi.i2c.I2cPort
 
     def __init__(self, i2c_port: pyftdi.i2c.I2cPort):
         self.i2c_port = i2c_port
@@ -16,21 +40,21 @@ class atsha204A:
         """Sends a wake message to the ATSHA204A IC"""
         self.i2c_port.write(0x00)
 
-    def sendCommand(self, oppcode: int, param1: int, param2: int,
-                    data: "list[int]", response_length: int):
+    def sendCommand(self, oppcode: atasha204A_command, param1: int,
+                    param2: int, data: "list[int]", response_length: int):
         """Sends a command to the Chip and recieves a response"""
 
         command = [
             0x03,  # command flag
             0x07 + len(data),  # count (includes fixed values)
-            oppcode,  # command oppcode
+            oppcode.value,  # command oppcode
             param1,  # paramter 1 value
-            param2 >> 8,  # paramater 2 value MSB
-            param2 & 0xff  # parameter 2 value LSB
+            param2 & 0xff,  # paramater 2 value LSB
+            param2 >> 8  # parameter 2 value MSB
         ]
         command += data  # add data to command
 
-        crc = atsha204A.crc16(command, 1, len(command) - 1)
+        crc = atsha204A.calculate_crc(command, 1, len(command) - 1)
 
         command += [
             crc & 0xFF,
@@ -42,7 +66,7 @@ class atsha204A:
         # todo check incoming CRC
         return self.i2c_port.read(response_length)
 
-    def crc16(data: bytearray, offset: int, length: int):
+    def calculate_crc(data: "bytearray|list[int]", offset: int, length: int):
         """Calculates the CRC for the ATSHA204A
         (poly 0805, start at 0x0000, reflect input,
         dont reflect output, no output exor)"""
@@ -64,21 +88,44 @@ class atsha204A:
         return crc & 0xFFFF
 
 #
-# Top level commands
+# ATSHA204A crypto commands
 #
 
-    def checkChipID(self):
-        # read intial memory block
+    def command_read(self, zone: atasha204A_zone, block: int, 
+                     offset: int, four_byte=False):
+        """Reads a block of memory from the IC"""
+
+        param1 = zone.value
+        if not four_byte:
+            param1 += 0x80  # 32 byte read flag
+
+        param2 = block << 3 + offset
+
         mem = self.sendCommand(
-            0x02,  # read command
-            0x80,  # 32 bytes from data block
-            0x0000,  # read fist four bytes
-            [],  # no data
+            atasha204A_command.READ,  # read command
+            param1, param2, [],
             38)
 
-        print(''.join('{:02x}'.format(x) for x in mem))
+        if four_byte:
+            return mem[1:5]
+        else:
+            return mem[1:33]
 
-        if (mem[1] == 0x01 and mem[2] == 0x23 and mem[13] == 0xEE):
+#
+# Top level commands
+#
+    def get_serial_number(self) -> bytearray:
+        """Gets the serial number from the ATSHA204A"""
+        mem = self.command_read(atasha204A_zone.CONFIG, 0, 0)
+
+        serial_number = mem[0:4] + mem[8:13]
+        return serial_number
+
+    def checkChipID(self):
+        """Checks if the Fixed bytes in the serial are correct"""
+        serial = self.get_serial_number()
+
+        if (serial[0] == 0x01 and serial[1] == 0x23 and serial[8] == 0xEE):
             return True
         else:
             return False
--- main.py
+++ main.py
@@ -2,10 +2,17 @@ import provisioner
 import atsha204a
 
 
+def hexString(data):
+    """Converts data into a hex string for printing"""
+    return ' '.join('{:02x}'.format(x) for x in data)
+
+
 def checkCypto(provisioner: provisioner.provisioner):
     crypto = atsha204a.atsha204A(provisioner.get_i2c_port(0xC8, shift=True))
     crypto.sendWake()
-    crypto.checkChipID()
+    print(hexString(crypto.get_serial_number()))
+    if crypto.checkChipID():
+        print("Serial is for ATSHA204A")
 
 
 if __name__ == "__main__":
